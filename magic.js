57bbc59f80f95f95dbfaf8716ce7d927d8142342
Siddhardha Gunupuru <ideasbag31@gmail.com>
2024-11-21 11:01:49 +0530

    Create magic.js

---
+++ magic.js
@@ -0,0 +1,29 @@
+const luckyNum = prompt("Enter your lucky number between 0 - 99.....");
+
+if (confirm("Are you Male ?")) {
+  console.log("CONGRATULATIONS YOU ARE MALE ♂👨")
+} else {
+  (confirm("Are you Female ?"));
+  console.log("CONGRATULATIONS YOU ARE FEMALE ♀👸")
+}
+
+if (confirm("ARE YOU INDIAN ?")) {
+  console.log("🇮🇳 ADVANCE HAPPY REPUBLIC DAY 🇮🇳")
+}
+
+if (confirm("DO YOU HAVE BLACK DRESS ?")) {
+  console.log("WEAR IT")
+} else {
+  console.log("THEN BUY IT")
+}
+
+if (confirm("DO YOU KNOW THAT YOU DONT HAVE BRAIN ?")) {
+  console.log("'THANK GOD' YOU REALISED EARLY")
+} else {
+  console.log("'YOU DONT HAVE TIME' REALISE FAST...")
+}
+
+if (confirm("CAN I GUESS YOUR LUCKY NUMBER ?")) {
+  console.log(luckyNum);
+} 
+console.log("THANKS FOR PLAYING 'DONT COME AGAIN'")
